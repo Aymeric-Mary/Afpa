b7755412c0eb61ea0f4aa7dcdf9b597e4413ff51
Aymeric <aymeric.mary.pls@gmail.com>
2020-07-16 15:18:49 +0200

    Modifiacation de parcAuto.js

--- js/parcAuto.js
+++ js/parcAuto.js
@@ -4,6 +4,5 @@ let mercedes = new Car('AB-123-CD','Bleu',800,90,55,5);
 mercedes.repaint("Bleu");
 mercedes.refuel(50);
 mercedes.move(1000, 80);
-mercedes.refuel(40);
-console.log(mercedes.getMsg())
+console.log(mercedes.getMsg());
 console.log(mercedes.toString());
